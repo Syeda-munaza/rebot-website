17d088c3a305e53b123121b7c5b0704c880debac
Syeda Munaza <imunazaizhar@gmail.com>
2024-11-25 14:49:12 +0500

    Add files via upload

---
+++ robot.js
@@ -0,0 +1,77 @@
+let btn= document.querySelector("#btn")
+let content= document.querySelector("#content")
+let voice= document.querySelector("#voice")
+function speak(text){
+    let text_speak=new SpeechSynthesisUtterance(text)
+    text_speak.rate=1
+    text_speak.pitch=1
+    text_speak.volume=1
+    text_speak.lang="hi-GB"
+    window.speechSynthesis.speak(text_speak)
+}
+function wishMe(){
+    let day=new Date()
+    let hours=day.getHours()
+    if(hours>=0 && hours<12){
+        speak("Good Morning Mam")
+    }else if(hours>=12 && hours<16){
+        speak("Good Afternoon Mam")
+    }else{
+        speak("Good Evening Mam")
+    }
+}
+window.addEventListener('load',()=>{
+    wishMe()
+})
+let speechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition
+let recognition=new speechRecognition()
+recognition.onresult= (event)=>{
+    let currentIndex=event.resultIndex
+    let transcript=event.results[currentIndex][0].transcript
+    content.innerText=transcript
+    takeCommand(transcript.toLowerCase())
+}
+btn.addEventListener("click",()=>{
+    recognition.start()
+     btn.style.display="none"
+    voice.style.display="block"
+}
+)
+function takeCommand(message){
+     btn.style.display="flex"
+      voice.style.display="none"
+    if(message.includes("hello")|| message.includes("hey")){
+        speak("hello sir, what can i helo you?")
+    }
+    else if(message.includes("who are you")){
+        speak("I am virtual assistant, created by syeda munaza")
+    }else if(message.includes("open youtube")){
+        speak("Opening youtube...")
+        window.open("https://www.youtube.com")
+    }
+    else if(message.includes("open google")){
+        speak("Opening google..")
+        window.open("https://www.google.co.uk/")
+    }
+    else if(message.includes("open chatgpt")){
+        speak("opening chatgpt")
+        window.open("https://chatgpt.com/")
+    }
+    else if(message.includes("open calculator")){
+        speak("Opening calculator")
+        window.open("https://www.calculator.net/")
+    }
+    else if(message.includes("date")){
+        let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+        speak(date)
+    }
+    else if(message.includes("open facebook")){
+        speak("Opening facebook")
+        window.open("https://www.facebook.com/")
+    }
+    else{
+        let finalText="this is what i found on internet regarding"+ message.replace("andrea","")|| message.replace("india","")
+        speak(finalText)
+        window.open(`https://www.google.com/search?q=${message.replace("andrea","")}`, "_blank")
+    }
+}
